c5647ed9a7e80f7872ccc5385ef1eed6665ff1c0
J.E. Alcalá <jaime.atemores@alumnos.udg.mx>
2022-08-26 09:57:48 -0500

    description corrected

--- R/marginal_plot.R
+++ R/marginal_plot.R
@@ -6,15 +6,6 @@ marginal_plot <- function(x, y,
                           bw = "nrd0", adjust = 1,
                           alpha = 1, plot_legend = T, ...) {
   require(scales)
-  ###############
-  # Plots a scatterplot with marginal probability density functions for x and y.
-  # Data may be grouped or ungrouped.
-  # For each group, a linear fit can be plotted. It is hidden by default, but can be shown by providing lm_show = TRUE.
-  # The model can be modified using the 'lm_formula' argument.
-  # The 'bw' and 'adjust' argument specify the granularity used for estimating probability density functions. See ?density for more information.
-  # For large datasets, opacity may be decreased by setting alpha to a value between 0 and 1.
-  # Additional graphical parameters are passed to the main plot, so you can customize axis labels, titles etc.
-  ###############
   moreargs <- eval(substitute(list(...)))
 
   # prepare consistent df
