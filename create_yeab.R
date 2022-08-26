e63ca10f3a7b37856cf0caa0671f9678f1b46bde
J.E. Alcalá <jaime.atemores@alumnos.udg.mx>
2022-08-26 09:58:27 -0500

    creat_yeab.R

---
+++ create_yeab.R
@@ -0,0 +1,30 @@
+library(usethis)
+library(devtools)
+
+use_description(
+  fields = list(
+    Title = "Package To Analyze Data From Analysis Of Behavior Experiments",
+    Description = "This is a colletion of functions aimed to analyze data from behavioral
+                   experiments from MED output and others. It also have functions to fit exponential or
+                   hyperbolic models from delay discounting tasks, exponential mixtures to IRTs, Gaussian
+                  plus ramp model for peak procedures data, etc.",
+    `Authors@R` = c(
+      person(
+        "Emmanuel", "Alcala",
+        email = "jealcala@gmail.com",
+        role = c("aut", "cre")
+      ),
+      person(
+        "Rodrigo", "Sosa",
+        email = "rsosas@up.edu.mx",
+        role = "aut"
+      )
+    ),
+    License = "CC0"
+  )
+)
+
+
+pck_import <- c("dplyr", "minpack.lm", "scales", "ggplot2", "VGAM", "grid", "gridExtra")
+
+sapply(pck_import, use_package)
