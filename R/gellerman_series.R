9fabcc73176924aed0b08a712ea21fabd3d5bae5
J.E. Alcalá <jaime.atemores@alumnos.udg.mx>
2022-09-15 11:33:32 -0500

    Update gellerman_series.R

--- R/gellerman_series.R
+++ R/gellerman_series.R
@@ -5,29 +5,43 @@
 # Creada: abril 4, 2019
 # Autor: Emmanuel Alcalá
 
-gell_like = function(n){ # n debe ser un número par
+#' Gellerman-like series
+#'
+#' @param n numeric, a vector of 0 and 1 (see Details)
+#'
+#' @return
+#' @export
+#'
+#' @details
+#' @examples
+gell_like <- function(n) { # n debe ser un número par
+
+  if (n %% 2 != 0) stop("n should be even (2, 4, ...")
+
+  # obtener la mitad de la longitud de n
+  half <- n / 2
   # Vector de 0s de tamaño n
-  half = n/2
-  gell = numeric(n)
+  gell <- numeric(n)
   # Llenar primeras 3 posiciones
-  gell[1:3] = rbinom(3,1,0.5)
-  while(sum(gell == 0) != half & sum(gell == 1) != half){
-    for(i in 4:n){ # comenzar en cuarta posición
-      gell[i] = rbinom(1,1,0.5)
+  gell[1:3] <- rbinom(3, 1, 0.5)
+  # se sale del while cuando la cantidad de 0s ó 1s es la mitad
+  while (sum(gell == 0) != half & sum(gell) != half) {
+    for (i in 4:n) { # comenzar en cuarta posición
+      gell[i] <- rbinom(1, 1, 0.5)
       # Si las últimos 4 valores suman 4
       # o si los últimos 4 valores suman 0,
       # recalcular el último valor
-      while(sum(gell[(i-3):i]) == 4 | sum(gell[(i-3):i]) == 0 ){
-        gell[i] = rbinom(1,1,0.5)
+      while (sum(gell[(i - 3):i]) == 4 | sum(gell[(i - 3):i]) == 0) {
+        gell[i] <- rbinom(1, 1, 0.5)
       } # continuar
     }
   }
   # retornar el vector de 0 y 1
-  
+
   gell
 }
 
-## Ejemplo: hace chunks de 12 columnas con 120 ensayos por fila y los apila 
+## Ejemplo: hace chunks de 12 columnas con 120 ensayos por fila y los apila
 # por columna. Es decir, primero hace 120x12, los apila, luego
 # otros 12 etc
 
@@ -35,20 +49,20 @@ gell_like = function(n){ # n debe ser un número par
 # ensayos = 30
 # reps = 2
 # matrix_gell1 = matrix(0,nrow = sesiones,ncol = ensayos)
-# 
+#
 # matrix_gell = matrix(0,nrow = sesiones,ncol = ensayos * reps)
 # k = 1
 # for (jj in 1:reps){
 #   for(ii in 1:sesiones){
 #     matrix_gell1[ii, ] = gell_like(ensayos)
 #   }
-#   
+#
 #   matrix_gell[ ,k:(jj*ensayos)] = matrix_gell1
 #   k = k + ensayos
-# 
+#
 # }
-# 
+#
 # path_comp = "C:/Users/Laboratorio_Jonathan/Dropbox/Dropbox/Emmanuel/rats/"
-# 
+#
 # write.table(matrix_gell,file = paste0(path_comp,'components_distr.txt'),
 #             sep = ",", col.names = F, row.names = F)
